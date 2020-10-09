116f45689529a3e897d0bca891ec655ab85abe6f
MarjaCarty <marja.d.carty@gmail.com>
2020-10-09 12:58:08 -0500

    Styled characters

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,14 +1,23 @@
 // Write your Character component here
 import React from "react";
+import styled from "styled-components";
+
+const StyledCharacter = styled.div`
+  background-color: white;
+  border-radius: 7px;
+  width: 60%;
+  padding: 3%;
+  margin: 3% auto;
+`;
 
 export default function Character(props) {
   const { name, sprite, type1, type2 } = props;
 
   return (
-    <div>
+    <StyledCharacter>
       <h1>{name}</h1>
       <img src={sprite} />
       <h3>{type1 + (type2 ? "/" + type2 : "")}</h3>
-    </div>
+    </StyledCharacter>
   );
 }
